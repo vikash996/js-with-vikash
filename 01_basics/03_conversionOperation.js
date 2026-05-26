de799aaa1149467b00a298652f099e281f1f49a6
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-05-26 02:13:45 +0000

    add 03_conversionObject.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let vlaueNumber = Number(score);
+// console.log(typeof vlaueNumber );
+
+// "33" => 33
+// "33abc" => NaN(not a number)
+// true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);// true
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let SomeNumber = 33
+let stringNumber = String(SomeNumber)
+console.log(stringNumber); //33
+console.log(typeof stringNumber); //33
+
+
+
+

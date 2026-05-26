f17d9733c360e3bc0320985c16f2f6171206edec
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-05-26 01:45:38 +0000

    Add variables and dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "vikash@gmail.com"
+var accountPass = "35453"
+accontCity = "Banda" 
+let accountState;
+
+// accountId =2    Not allowed
+
+accountEmail = "vikashPanday@gmail.com"
+accountPass="6566660"
+accontCity="ujjain"
+
+console.log(accountState);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 +  3 )  we are using nodejs not browsers
+
+console.log("vikash");
+let name = "vikash";
+let age = 21;
+let isLoggedIn = false;
+
+// number =>2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value  null is object 
+// undifined is a type 
+// symbol => for indentify unique
+
+
+// object 
+console.log(typeof "vikash");
+
+

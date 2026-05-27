80e8f4a9f7258e13721f1431235ec9d40f0d75b2
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-05-27 01:52:08 +0000

    string add

---
+++ 01_basics/05_String.js
@@ -0,0 +1,39 @@
+let name = "vikash panday"
+let repo = 50;
+
+console.log(`my name is ${name} and i have ${repo} repo on github`);
+
+const gameName = new String('vikassshh')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+
+
+// console.log(typeof gameName);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,5)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,5)
+// console.log(anotherString);
+
+// const newStringOne = "     vikash     " 
+// console.log(newStringOne)
+// console.log(newStringOne.trim());
+
+
+// const url = "https://www.youtube.com/vikashpandaydotcom"
+// console.log(url.replace("https","http"));
+
+// console.log(url.includes("vikash"));
+// console.log(url.includes("hello"));
+
+console.log(gameName.split("a"));
+
+
+

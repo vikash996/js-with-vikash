0a2770409588276e2216157d23c9bf6feddd0b7c
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-05-27 01:18:07 +0000

    dataadded

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -34,3 +34,42 @@ const myFunction = function(){
 }
 
 console.log(typeof myFunction);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(Primitive)  ,  Heap (Non- Primitive)
+  
+// stack basically cophy deta hai na ki original refrence
+
+let myYoutubename = "vikashPandaydotcom"
+
+let anothername = myYoutubename
+anothername = "pandayJivlogs"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+// heap me referece ka oroginal address 
+// hota hai jo ki ek bar banaya jata hai 
+// jab hum object create karte hai , aur 
+// jab hum usko dusre variable me assign
+//  karte hai to wo usi address ko point 
+// karta hai , isliye jab hum usme change
+//  karte hai to dono me change dikhai deta
+//  hai kyuki dono same address ko point kar 
+// rahe hote hai
+let userOne = {
+    name:"vikash",
+    email:"vikashPanday@gmail.com"
+}
+
+let userTwo = userOne
+userTwo.email = "panday9005@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
+
+
+
+

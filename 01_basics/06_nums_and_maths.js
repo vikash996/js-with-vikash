ee3201ca1cb3a3b06f376fff1f57267ebc8c7444
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-05-30 02:50:07 +0000

    adding

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number(1000);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.456789;
+// console.log(otherNumber.toPrecision(4))// if value is less than number it will give exponential value redirectly 
+
+// const anotherNumber = 10000000;
+// console.log(anotherNumber.toLocaleString('en-IN'))
+
+//++++++++++++++  MATHS  +++++++++++++++++++++++
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// const area = 1;
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random())
+// console.log(Math.random()*10);
+
+// console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);

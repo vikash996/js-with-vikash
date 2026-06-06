1c1531df61aa59af511734cd8b5fa8ad1c13027c
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-06-06 07:10:33 +0000

    added twwo

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// while loops 
+
+// let i =0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// let myArry = ["vikash", "subhash", "ram", "shyam"];
+// let i=0;
+// while(i<myArry.length){
+//     console.log(myArry[i]);
+//     i++;
+    
+// }
+
+// do 
+
+let score =7;
+do {
+    console.log(score)
+    score++;
+} while (score<=10);

82347a805efed2ed0f43a2080164a8bab332dd4d
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-06-06 07:49:21 +0000

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+// for or 
+const arr = [1,2,3,4,5];
+for(const i of arr){
+    // console.log(i);
+}
+
+
+const greetings = "hello vikash!";
+for(const i of greetings){
+    // console.log(i);
+}
+
+// map
+
+const map = new Map();
+map.set('IN',"india")
+map.set('USA',"UNITED STATE OF AMERICA")
+map.set('FR',"FRANCE");
+// console.log(map);
+for (const i of map){
+    // console.log(map);
+}
+
+
+const myobj = {
+    game : "temple run",
+    player: "yashuka"
+
+}
+for(const i of myobj){
+    console.log(i)
+}

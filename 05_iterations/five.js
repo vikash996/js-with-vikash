5ebe835b50acc7646e5dd2a5d26ccfac440656bb
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-06-06 07:50:24 +0000

    five

---
+++ 05_iterations/five.js
@@ -0,0 +1,39 @@
+const laguages = ["js", "java", "html", "css"];
+
+
+// laguages.forEach( function (val){
+//     console.log(val);
+// })
+
+
+// laguages.forEach( (val) => {
+//     console.log(val)
+// })
+
+// function pritme (val){
+//     console.log(val);
+// }
+// console.log(pritme);
+
+// laguages.forEach( (item, index ,arr)=> {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        mylaguages : "js",
+        filename : "javascript"
+    },
+    {
+        mylaguages : "py",
+        filename : "python"
+    },
+    {
+        mylaguages : "java",
+        filename : "java"
+    },
+]
+
+myCoding.forEach( (item)=> {
+    console.log(item.mylaguages);
+})

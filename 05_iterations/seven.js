db31e21c4263843e8cb4437ae2ec5e38f246f589
vikash Panday <vikash.panday_cs24@gla.ac.in>
2026-06-12 16:27:24 +0000

    added

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+const myNums = [1,2,3,4,5,6,7,6];
+// const newNums = myNums.forEach( (num) => {
+//     return num+10;
+// })
+// console.log(newNums);
+
+// const newNums = myNums.map( (num) => {
+//     return num+10;
+// });
+// console.log(newNums);
+
+const newNums = myNums.map( (num) => num * 10 ).map( (num) => num + 1 ).filter( (num) => num >= 40);
+
+console.log(newNums);
